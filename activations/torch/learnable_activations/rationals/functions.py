7f1254143883bc3898f115e652cea0f349d11fa4
aff3npirat <jurek.elliesen@web.de>
2024-08-23 15:18:35 +0200

    Add Documentation

--- activations/torch/learnable_activations/rationals/functions.py
+++ activations/torch/learnable_activations/rationals/functions.py
@@ -3,6 +3,22 @@ import torch
 
 
 def rational_A(x, weight_numerator, weight_denominator):
+    """Computes :math:`f(x)=\\frac{\\sum_i^Na_ix^i}{1+\\sum_i^M|b_ix^i|}`.
+    
+    Args:
+        x (:class:`torch.Tensor`):
+            Inputs of any shape. Will be treated as 1D tensor.
+
+        weight_numerator (:class:`torch.Tensor`):
+            Tensor of shape :math:`N`.
+
+        weight_denominator (:class:`torch.Tensor`):
+            Tensor of shape :math:`M`.
+
+    Returns:
+        :class:`torch.Tensor`:
+            Tensor of same shape as ``x``.
+    """
     len_num, len_deno = len(weight_numerator), len(weight_denominator)
     x_powers = torch.pow(x.view(-1, 1), torch.arange(max(len_num, len_deno + 1), device=x.device))
     numerator = torch.mul(x_powers[:, :len_num], weight_numerator).sum(1)
@@ -11,6 +27,22 @@ def rational_A(x, weight_numerator, weight_denominator):
 
 
 def rational_B(x, weight_numerator, weight_denominator):
+    """Computes :math:`f(x)=\\frac{\\sum_i^Na_ix^i}{1+|\\sum_i^Mb_ix^i|}`.
+    
+    Args:
+        x (:class:`torch.Tensor`):
+            Inputs of any shape. Will be treated as 1D tensor.
+
+        weight_numerator (:class:`torch.Tensor`):
+            Tensor of shape :math:`N`.
+
+        weight_denominator (:class:`torch.Tensor`):
+            Tensor of shape :math:`M`.
+
+    Returns:
+        :class:`torch.Tensor`:
+            Tensor of same shape as ``x``.
+    """
     len_num, len_deno = len(weight_numerator), len(weight_denominator)
     x_powers = torch.pow(x.view(-1, 1), torch.arange(max(len_num, len_deno + 1), device=x.device))
     numerator = torch.mul(x_powers[:, :len_num], weight_numerator).sum(1)
@@ -19,6 +51,22 @@ def rational_B(x, weight_numerator, weight_denominator):
 
 
 def rational_C(x, weight_numerator, weight_denominator):
+    """Computes :math:`f(x)=\\frac{\\sum_i^Na_ix^i}{|\\sum_i^Mb_ix^i|}`.
+    
+    Args:
+        x (:class:`torch.Tensor`):
+            Inputs of any shape. Will be treated as 1D tensor.
+
+        weight_numerator (:class:`torch.Tensor`):
+            Tensor of shape :math:`N`.
+
+        weight_denominator (:class:`torch.Tensor`):
+            Tensor of shape :math:`M`.
+
+    Returns:
+        :class:`torch.Tensor`:
+            Tensor of same shape as ``x``.
+    """
     len_num, len_deno = len(weight_numerator), len(weight_denominator)
     x_powers = torch.pow(x.view(-1, 1), torch.arange(max(len_num, len_deno), device=x.device))
     numerator = torch.mul(x_powers[:, :len_num], weight_numerator).sum(1)
@@ -27,10 +75,35 @@ def rational_C(x, weight_numerator, weight_denominator):
 
 
 def rational_D(x, weight_numerator, weight_denominator, *, noise_deviation=0.2):
+    """Computes version **B** but with noise added to numerator.
+
+    .. math::
+        \\begin{cases}
+            \\text{let }c\\text{be sampled from uniform distribution }\\mathcal{U} \\\\
+            f(x)=\\frac{\\sum_i^Na_ix^ic_i}{1+|\\sum_i^Mb_ix^i|}
+        \\end{cases}
+    
+    Args:
+        x (:class:`torch.Tensor`):
+            Inputs of any shape. Will be treated as 1D tensor.
+
+        weight_numerator (:class:`torch.Tensor`):
+            Tensor of shape :math:`N`.
+
+        weight_denominator (:class:`torch.Tensor`):
+            Tensor of shape :math:`M`.
+
+        noise_deviation (float):
+            Specifies noise distribution :math:`\\mathcal{U}=\\frac{1}{2\\sigma}`.
+
+    Returns:
+        :class:`torch.Tensor`:
+            Tensor of same shape as ``x``.
+    """
     len_num, len_deno = len(weight_numerator), len(weight_denominator)
     x_powers = torch.pow(x.view(-1, 1), torch.arange(max(len_num, len_deno), device=x.device))
 
-    noise = torch.FloatTensor(len_num).uniform_(1-noise_deviation, 1+noise_deviation)
+    noise = torch.FloatTensor(len_num).uniform_(1.0-noise_deviation, 1.0+noise_deviation)
     noised_numerator = torch.mul(weight_numerator, noise)
 
     numerator = torch.mul(x_powers[:, :len_num], noised_numerator).sum(1)
@@ -39,6 +112,25 @@ def rational_D(x, weight_numerator, weight_denominator, *, noise_deviation=0.2):
 
 
 def rational_nonsafe(x, weight_numerator, weight_denominator):
+    """Computes :math:`f(x)=\\frac{\\sum_i^Na_ix^i}{1+\\sum_i^Mb_ix^i}`.
+    
+    Args:
+        x (:class:`torch.Tensor`):
+            Inputs of any shape. Will be treated as 1D tensor.
+
+        weight_numerator (:class:`torch.Tensor`):
+            Tensor of shape :math:`N`.
+
+        weight_denominator (:class:`torch.Tensor`):
+            Tensor of shape :math:`M`.
+
+    Returns:
+        :class:`torch.Tensor`:
+            Tensor of same shape as ``x``.
+
+    .. warning::
+        Can result in division by zero.
+    """
     len_num, len_deno = len(weight_numerator), len(weight_denominator)
     x_powers = torch.pow(x.view(-1, 1), torch.arange(max(len_num, len_deno + 1), device=x.device))
     numerator = torch.mul(x_powers[:, :len_num], weight_numerator).sum(1)
@@ -47,6 +139,25 @@ def rational_nonsafe(x, weight_numerator, weight_denominator):
 
 
 def rational_spline(x, weight_numerator, weight_denominator, *, k=2.0):
+    """Computes :math:`f(x)=\\frac{\\sum_i^Na_ix^i\\cdot\\text{max}\\{0, x_i+k\\}\\cdot\\text{min}\\{0, x_i-k\\}}{1+|\\sum_i^Mb_ix^i|}`.
+    
+    Args:
+        x (:class:`torch.Tensor`):
+            Inputs of any shape. Will be treated as 1D tensor.
+
+        weight_numerator (:class:`torch.Tensor`):
+            Tensor of shape :math:`N`.
+
+        weight_denominator (:class:`torch.Tensor`):
+            Tensor of shape :math:`M`.
+
+        k (float):
+            Parameter :math:`k`.
+
+    Returns:
+        :class:`torch.Tensor`:
+            Tensor of same shape as ``x``.
+    """
     len_num, len_deno = len(weight_numerator), len(weight_denominator)
     x_powers = torch.pow(x.view(-1, 1), torch.arange(max(len_num, len_deno + 1), device=x.device))
     z = x.view(-1)
